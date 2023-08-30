9daefd229eb097fc148f405280a4f0dc6ca5454d
Khabir <hakim1khabir2@gmail.com>
2023-08-30 16:39:48 +0100

    Add resources

---
+++ public/assets/CARTS.js
@@ -0,0 +1,60 @@
+const CARTS = [
+  {
+    img_url:
+      "https://ae01.alicdn.com/kf/Sbf275334291e43dab12a007524e42d1fw/Drawing-Tablet-XPPen-7x4inch-Pen-Tablet-Deco-Mini7-with-Tilt-Function-8-Keys-8192-Levels-Graphics.jpg_220x220xz.jpg_.webp",
+    description:
+      "Drawing Tablet XPPen 7x4inch Pen Tablet Deco Mini7 with Tilt Function 8 Keys 8192 Levels Graphics Tablet for Android Mac Windows",
+    starts: 4.9,
+    reviews: 203,
+    sold: 900,
+    price: 29.18,
+    brand: "XPPen",
+  },
+  {
+    img_url:
+      "https://ae01.alicdn.com/kf/S6ea1383f368d4ae6bae4bbccecae0386G/HUION-H430P-Digital-Tablets-Micro-USB-Signature-Graphics-Drawing-Pen-Tablet-OSU-Game-Battery-Free-Tablet.jpg_220x220xz.jpg_.webp",
+
+    description:
+      "HUION H430P Digital Tablets Micro USB Signature Graphics Drawing Pen Tablet OSU Game Battery-Free Tablet Android Mac Windows",
+    starts: 4.8,
+    reviews: 17,
+    sold: 117,
+    price: 25.04,
+    brand: "HUION",
+  },
+  {
+    img_url:
+      "https://ae01.alicdn.com/kf/S37e7d25c5c0744bfafd19db296b770ccR/XPPen-Drawing-Tablet-Graphics-StarG960S-Plus-9x6-Inch-Pen-Tablet-with-4-Keys-Support-Learning-8192.jpg_220x220xz.jpg_.webp",
+
+    description:
+      "XPPen Drawing Tablet Graphics StarG960S Plus 9x6 Inch Pen Tablet with 4 Keys Support Learning 8192 Levels for Windows Mac AndroiUION H430P Digital Tablets Micro USB Signature Graphics Drawing Pen Tablet OSU Game Battery-Free Tablet Android Mac Windows",
+    starts: 4.9,
+    reviews: 17,
+    sold: 67,
+    price: 38.28,
+    brand: "XPPen",
+  },
+  {
+    img_url:
+      "https://ae01.alicdn.com/kf/S186091a292844b109e9836a7807b60adn/Original-CTL-672-For-Wacom-One-by-Wacom-Digital-Graphic-Drawing-Tablet-Creative-Pen-Medium-CTL672.jpg_220x220xz.jpg_.webp",
+
+    description:
+      "Original CTL-672 For Wacom One by Wacom Digital Graphic Drawing Tablet Creative Pen Medium CTL672/K2",
+    starts: 4.9,
+    reviews: 5,
+    sold: 19,
+    price: 84.29,
+    brand: "Wacom",
+  },
+];
+
+export const BRANDS_IMGS = {
+  HUION:
+    "https://ae01.alicdn.com/kf/He7daf1a97ded49dbac921dab98138a5de.jpg_q90.jpg_.webp",
+  Wacom:
+    "https://upload.wikimedia.org/wikipedia/en/thumb/7/7e/Wacom_Logo_WhiteType.svg/220px-Wacom_Logo_WhiteType.svg.png",
+  XPPen:
+    "https://www.xp-pen.com/Uploads/images/2022/09/1662601083a65a51120ec710ab.svg",
+};
+
+export default CARTS;
